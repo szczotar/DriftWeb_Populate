a8300f209bf7041f28668d2ec8a95a379713f496
szczotar <szczotm>
2021-03-06 21:36:57 +0100

    hidden selectors

--- DriftWeb_populate_queue.py
+++ DriftWeb_populate_queue.py
@@ -1,16 +1,17 @@
+  
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
-
+from selenium.common.exceptions import ElementNotInteractableException, TimeoutException
+from selenium.webdriver.support.select import Select
 
 
 # from selenium.webdriver.support.ui import Select
 
 import time
-
 
 
 
@@ -21,13 +22,14 @@ class Populate_queue():
         
         options = webdriver.ChromeOptions()
         options.add_argument("--start-maximized")
-        self.driver = webdriver.Chrome(r"C:\Users\admin\Desktop\Env\new_env\Web_automation_project1\chromedriver.exe",options=options)
+        self.driver = webdriver.Chrome(options=options)
         self.driver.get("https://preprod.dw3.dk/")
         self.action = ActionChains(self.driver)
   
-        
         self.log_in()
         self.navigate_to_list("PARK","sfsaf")
+        self.select_tab()
+        self.collect_tasks()
 
     
     def log_in(self):
@@ -55,23 +57,89 @@ class Populate_queue():
         self.Arbejdsgruppe = Arbejdsgruppe
 
         try:
-            time.sleep(2)
-            filter  = self.driver.find_element(By.ID,"navbarChangeClient")
-            filter.click()
-            head = self.driver.find_element(By.XPATH,"""//*[@id="changeClientList"]/div[1]/div[3]/span""")
-
-            point = head.location
-            print(point)
-            time.sleep(2)
-            self.action.move_to_element_with_offset(filter,50,140).double_click().perform()
             
+            # filter  = self.driver.find_element(By.ID,"navbarChangeClient")
+            filter = WebDriverWait(self.driver,20).until(
+                EC.visibility_of_element_located((By.ID,"navbarChangeClient"))
+                )
+            filter.click()
+
+
+            element1= self.driver.find_element_by_xpath("""//*[@id="spMenu"]""")
+            print(self.driver.execute_script("arguments[0].getText;",element1))
+
+            string1 = str("javascript:document.getElementById('spMenu').click();")
+            self.driver.execute_script(string1)
+
+
+            time.sleep(5)
+            self.action.send_keys(Keys.DELETE).perform()
+            self.action.send_keys(Keys.DOWN*2).perform()
+            time.sleep(2)
+            self.action.send_keys(Keys.ENTER).perform()
+            time.sleep(2)
+            # string2 = ("javascript:document.getElementById('changeclientwgchecklist_ms').click();")
+            # self.driver.execute_script(string2)
+            time.sleep(5)
+            opt = self.driver.find_element(By.ID,"ui-multiselect-0-changeclientwgchecklist-option-3")
+            opt.click()
+            time.sleep(5)
+
+            button = self.driver.find_element(By.ID,"changeClientButton")
+            button.click()
         finally:
             pass
+
+    def select_tab(self):
+        try:
+            
+            Opgaver = WebDriverWait(self.driver,10).until(
+                EC.visibility_of_element_located((By.ID,"taskToggleNavigation"))
+                )
+            Opgaver.click()
+           
+            Godkendt = WebDriverWait(self.driver,10).until(
+                EC.visibility_of_element_located((By.ID,"nav_task_6"))
+                )
+                
+            Godkendt.click()
+
+        except:
+            # self.driver.find_element(By.ID,"taskToggleNavigation").click()
+            # time.sleep(2)
+            # self.action.move_to_element(Godkendt)
+            self.driver.find_element(By.ID,"nav_task_6").click()
+            
+
+        finally:
+            pass
+
+    def collect_tasks(self):
+        try:
+            row = 1
+            T_numbers =[]
+            while True:
+            
+                task = WebDriverWait(self.driver,20).until(
+                    EC.presence_of_element_located((By.XPATH,f"""//*[@id="list-datatable"]/tbody/tr[{row}]/td[2]"""))
+                )
+
+                # task = self.driver.find_element(By.XPATH,f"""//*[@id="list-datatable"]/tbody/tr[{row}]/td[2]""")
+            
+                t =task.get_attribute("innerText")
+                print(t)
+    
+                T_numbers.append(t)
+                
+                row += 1
+
+            
+
+        except TimeoutException:
+            print("No more element")
+            print(T_numbers)
 
 
             
 if __name__ == "__main__" :
-    queue = Populate_queue()
-
-
-
+    queue = Populate_queue("")
